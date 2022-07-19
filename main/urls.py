e59c2c476aa754304bf7d397d853c7e08059fb7c
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-07-19 15:38:49 +0300

    added the contact view path

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from main import views
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name='home'),
     path("about-us/", TemplateView.as_view(template_name="about.html"), name='about-us'),
+    path("contact-us/", views.ContactUsView.as_view(template_name="contact.html"),
+         name='contact-us'),
+
 ]
